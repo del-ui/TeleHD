a9e862100325ea91ee142eaf55a28ff75aa9e42c
Kidrapper <wambuivictor@students.must.ac.ke>
2022-02-28 10:10:37 +0300

    login

--- app/src/main/java/com/victor/telehd/MainActivity.java
+++ app/src/main/java/com/victor/telehd/MainActivity.java
@@ -3,6 +3,12 @@ package com.victor.telehd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView username = (TextView) findViewById(R.id.username);
+        TextView password = (TextView) findViewById(R.id.password);
+
+        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
+
+        // username victor password fab123
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (username.getText().toString().equals("victor") && password.getText().toString().equals("fab123")){
+                    //correct
+                    Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
+                }else
+                    //incorrect credentials
+                    Toast.makeText(MainActivity.this, "LOGIN ERROR !!! Credentials Don't match! ", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
