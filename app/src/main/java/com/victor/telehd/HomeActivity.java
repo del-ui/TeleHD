06fe4167fa4c0c7f6b8ec4ca56f918b1004872e6
Kidrapper <wambuivictor@students.must.ac.ke>
2022-03-15 11:05:13 +0300

    update

--- app/src/main/java/com/victor/telehd/HomeActivity.java
+++ app/src/main/java/com/victor/telehd/HomeActivity.java
@@ -1,6 +1,6 @@
 package com.victor.telehd;
 
-import androidx.annotation.NonNull;
+ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
--- app/src/main/java/com/victor/telehd/MainActivity.java
+++ app/src/main/java/com/victor/telehd/MainActivity.java
@@ -1,91 +1,17 @@
 package com.victor.telehd;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.button.MaterialButton;
-
-public class MainActivity extends AppCompatActivity {
-    GoogleSignInOptions gso;
-    GoogleSignInClient gsc;
-    ImageView googleBtn;
-
+class MainActivity extends AppCompatActivity {
 
     @Override
+
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        googleBtn = findViewById(R.id.google_btn);
-
-        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
-        gsc = GoogleSignIn.getClient(this, gso);
-
-        googleBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                signIn();
-            }
-        });
-
-
-
-  /*      TextView username = (TextView) findViewById(R.id.username);
-        TextView password = (TextView) findViewById(R.id.password);
-
-        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
-
-        // username victor password fab123
-        loginbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (username.getText().toString().equals("victor") && password.getText().toString().equals("fab123")){
-                    //correct
-                    Toast.makeText(MainActivity.this, "LOGIN SUCCESSFUL", Toast.LENGTH_SHORT).show();
-                }else
-                    //incorrect credentials
-                    Toast.makeText(MainActivity.this, "LOGIN ERROR !!! Credentials Don't match! ", Toast.LENGTH_SHORT).show();
-            }
-        });
-    }*/
-    }
-    void signIn(){
-        Intent signInIntent = gsc.getSignInIntent();
-        startActivityForResult(signInIntent, 1000);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1000){
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-
-            try {
-                task.getResult(ApiException.class);
-                navigateToHomeActivity();
-            } catch (ApiException e) {
-                Toast.makeText(getApplicationContext(), "Something went wrong", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-    void navigateToHomeActivity(){
-        finish();
-        Intent intent = new Intent(MainActivity. this,HomeActivity.class);
-        startActivity(intent);
     }
 }
